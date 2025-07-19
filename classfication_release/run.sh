1b62d467049201ecdc0ed2760f487a84e4481dcc
fanqihang.2000 <fanqihang.2000@bytedance.com>
2025-07-19 08:44:57 +0000

    add code

---
+++ classfication_release/run.sh
@@ -0,0 +1,1 @@
+torchrun main.py --warmup-epochs 5 --model MALA_S  --data-path /your/path/to/imagenet  --num_workers 16  --batch-size 128  --drop-path 0.05  --epoch 300 --dist-eval  --output_dir /ckpt
